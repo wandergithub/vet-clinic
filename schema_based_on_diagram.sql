733e6ef8b7adcf4e17209d91f06d0b437ec00786
Aldo Aníbal Colombo <acolombo1@yahoo.com>
2022-07-12 17:35:46 -0300

    Correct tipos and table creation order

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,11 @@
+CREATE TABLE patients (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR,
+  date_of_birth DATE
+);
+
 CREATE TABLE medical_histories (
-  id INT PRIMARY KEY GENERATED ALWAYS AS DEFAULT,
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   admitted_at TIMESTAMP,
   patient_id INT,
   status VARCHAR,
@@ -7,36 +13,35 @@ CREATE TABLE medical_histories (
     FOREIGN KEY(patient_id)
       REFERENCES patients(id)
 );
+
 CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
 
-CREATE TABLE patients (
-  id INT PRIMARY KEY GENERATED ALWAYS AS DEFAULT,
-  name VARCHAR,
-  date_of_birth DATE
-);
 CREATE TABLE treatments (
   id INT PRIMARY KEY GENERATED ALWAYS AS DEFAULT,
   type VARCHAR,
   name VARCHAR
 );
+
+CREATE TABLE invoices (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id)
+);
+
 CREATE TABLE invoice_items (
-  id INT PRIMARY KEY GENERATED ALWAYS AS DEFAULT,
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   unit_price DECIMAL,
   quantity INT,
   total_price DECIMAL,
   invoice_id INT REFERENCES invoices(id),
   treatment_id INT REFERENCES treatments(id)
 );
+
 CREATE INDEX invoice_id_asc ON invoice_items(invoice_id ASC);
 CREATE INDEX treatment_id_asc ON invoice_items(invoice_id ASC);
 
-CREATE TABLE invoices (
-  id INT PRIMARY KEY GENERATED ALWAYS AS DEFAULT,
-  total_amount DECIMAL,
-  generated_at TIMESTAMP,
-  payed_at TIMESTAMP,
-  medical_history_id INT REFERENCES medical_histories(id), 
-);
 CREATE INDEX medical_histories_id_asc ON invoices(medical_history_id ASC);
 
 -- Join tables
